e3b7d86404654fad7e3629992c75e7428922ab87
mal-vka <malwina.g@gmial.com>
2022-03-18 23:55:19 +0100

    connection with db

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dataJSON = require("./data");
-// const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser"); // niepotrzebny - funkcja wbudowana w Expressa
 const { sha256 } = require("js-sha256");
 const mustacheExpress = require("mustache-express");
 const tools = require("./tools")
@@ -12,16 +12,25 @@ app.engine("mustache", mustacheExpress());
 app.set("view engine", "mustache");
 app.set("views", `${__dirname}/../views`);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); //zamiast body-parsera
+app.use(express.urlencoded({ extended: false })); //zamiast body-parsera
 app.use(express.static(`${__dirname}/../static`));
+
+const { Pool } = require("pg");
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "mr.coffee",
+    password: "123",
+    port: 5432,
+});
 
 
 // Adding form
 
-app.get("/schedules/new", (request, response) => {
-    response.render("form_newTerm", { users: tools.addId(dataJSON.users) });
-});
+// app.get("/schedules/new", (request, response) => {
+//     response.render("form_newTerm", { users: tools.addId(dataJSON.users) });
+// });
 
 app.get("/users/new", (request, response) => {
     response.render("form_newUser", {});
@@ -38,9 +47,10 @@ app.get("/users", (request, response) => {
     response.render("users", { users: tools.addId(dataJSON.users) });
 });
 
-app.get("/schedules", (request, response) => {
-    response.render("schedules", { schedules: dataJSON.schedules });
-});
+// 3b:
+// app.get("/schedules", (request, response) => {
+//     response.render("schedules", { schedules: dataJSON.schedules });
+// });
 
 app.get("/users/:url_id", (request, response) => {
     const user = dataJSON.users[request.params.url_id];
@@ -61,17 +71,17 @@ app.get("/users/:url_id/schedules", (request, response) => {
 
 // Adding users & terms
 
-app.post("/schedules", (request, response) => {
-    const schedule = request.body;
-    const scheduleObj = {
-        "user_id": schedule.user_id,
-        "day": schedule.day,
-        "start_at": schedule.start_at,
-        "end_at": schedule.end_at
-    };
-    dataJSON.schedules.push(scheduleObj);
-    response.redirect("/schedules");
-});
+// app.post("/schedules", (request, response) => {
+//     const schedule = request.body;
+//     const scheduleObj = {
+//         "user_id": schedule.user_id,
+//         "day": schedule.day,
+//         "start_at": schedule.start_at,
+//         "end_at": schedule.end_at
+//     };
+//     dataJSON.schedules.push(scheduleObj);
+//     response.redirect("/schedules");
+// });
 
 app.post("/users", (request, response) => {
     const user = request.body;
